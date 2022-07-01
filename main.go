2716c3184a01956434353655ccc340677a1a5b4f
rawand <26229485+r4wand@users.noreply.github.com>
2022-07-01 14:55:59 +0300

    Feat(): some playing around

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -8,15 +11,35 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Main window")
 
-	hello := widget.NewLabel("Hello Fyne!")
+	// this window will close all other windows when it is closed
+	w.SetMaster()
+
+	// resize the window to a specific size
+	w.Resize(fyne.NewSize(800, 600))
+
+	hello := widget.NewLabel("Hello Fyne")
+
 	w.SetContent(container.NewVBox(
+		widget.NewLabel("Hello Fyne!"),
 		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
+		widget.NewButton("New window", func() {
+			w2 := a.NewWindow("New window")
+			w2.SetContent(widget.NewLabel("Hello Fyne!"))
+			w2.Show()
+		}),
+		widget.NewButton("Quit", func() {
+			a.Quit()
 		}),
 	))
+	go func() {
+		for range time.Tick(time.Second) {
+			formatted := time.Now().Format("Time: 03:04:05:00")
+			hello.SetText(formatted)
+
+		}
+	}()
 
 	w.ShowAndRun()
 }
